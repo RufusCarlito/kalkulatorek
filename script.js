2532be6df20892da8e57380885b6e3436f4fff66
RufusCarlito <66885342+RufusCarlito@users.noreply.github.com>
2021-10-10 18:54:23 +0200

    v1.2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert('Najpierw wprowadz obwód i nacisnij "średnica"')
 function Calculate(){
     var obwod = Number(document.getElementById("obwod").value);
     var centyMniejsze = Number(document.getElementById("mniejsza wartosc").value);
@@ -15,6 +16,13 @@ function Calculate(){
     
 
     parseInt(document.getElementById("SUM").innerHTML = wynik.toFixed(2));
+    
     console.log(srednica, wartosc, roznica, stosunek, lataDodania,);
     console.log(obwod, centyMniejsze, centyWieksze);
+}
+function sred(){
+    var obwod = Number(document.getElementById("obwod").value);
+    var srednica = (obwod / 3.14);
+    parseInt(document.getElementById("sred").innerHTML = srednica.toFixed(2));
+    alert("teraz wprowadź resztę danych");
 }
